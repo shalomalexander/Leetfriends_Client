94315bf2a4fa09d6029561016a0bee87ed7e327c
unknown <shalomalexander68@gmail.com>
2023-02-16 16:44:01 +0530

    axios url updated

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_REACT_APP_BASE_URL,
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_REACT_APP_BASE_URL,
+  // baseURL: "http://localhost:3000",
 });
 
 export default api;
